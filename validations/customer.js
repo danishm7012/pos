e23ff2946c4b0e4fab57c7c5c4485edb7dee6b0f
abubakar936 <76734827+abubakar936@users.noreply.github.com>
2021-10-03 18:12:28 +0500

    required feilds removed

--- validations/customer.js
+++ validations/customer.js
@@ -3,10 +3,10 @@ function customer_Validation(customer) {
     const customer_Schema = Joi.object
         ({
             customer_name: Joi.string().required().min(1).max(120),
-            customer_email: Joi.string().required().min(1).max(120),
+            customer_email: Joi.string(),
             contact_no: Joi.string().required().min(1).max(120),
-            address: Joi.string().required().min(1).max(120),
-            previous_balance: Joi.number().required(),
+            address: Joi.string(),
+            previous_balance: Joi.number(),
             //   Customer_type: Joi.string().required().min(1).max(120),
         })
     return result = customer_Schema.validate(customer);
--- validations/supplier.js
+++ validations/supplier.js
@@ -4,9 +4,9 @@ function supplier_Validation(supplier) {
         ({
             supplier_name: Joi.string().required().min(1).max(120),
             contact_no: Joi.number().required(),
-            address: Joi.string().required().min(1).max(120),
-            supplier_email: Joi.string().required().min(1).max(120),
-            previous_balance: Joi.number().required(),
+            address: Joi.string(),
+            supplier_email: Joi.string(),
+            previous_balance: Joi.number(),
             //   owner_name: Joi.string().required().min(1).max(120),
         })
     return result = supplier_Schema.validate(supplier);
